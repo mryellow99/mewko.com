a2230cc978e42c289c993be9751eb21cb0994453
mryellow99 <ye01ll10ow@gmail.com>
2019-10-07 21:28:42 +0900

    中身追加

--- pages/Wants.vue
+++ pages/Wants.vue
@@ -1,8 +1,32 @@
 <template>
-  <article>
+  <article class="max-width">
     <h1>Wants</h1>
     <section>
-      <h2>私のやりたいこと</h2>
+      <h2>私のやりたいお仕事一覧</h2>
+      <ul>
+        <li>イラストのお仕事がしたい！！！！子供の頃からの夢その１</li>
+        <li>マンガを描くお仕事がしたい！！！子供の頃からの夢その２</li>
+        <li>Webサイトのコーディングがしたい！！これが一番実務経験がある。</li>
+        <li>子供に関わる仕事がしたい。一緒に遊んだり学んだり。</li>
+        <li>
+          カウンセリングのようなことをいつかしたい。悩める人の手助けをしたい
+        </li>
+        <li>農業やってみたい！日本の食料自給率を上げたい</li>
+        <li>カレー屋さんしてみたい。老後とかに。</li>
+      </ul>
     </section>
+    <img id="wants-image" src="~/assets/wants.png" height="600" />
   </article>
 </template>
+
+<style scoped>
+.max-width {
+  max-width: 800px;
+  margin: 50px auto;
+}
+#wants-image {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+</style>
